bf060c5c207ec1b7205bbe09370aaf8c13af631a
Franz Höpfinger <mail@franzhoepfinger.de>
2021-02-23 11:13:41 +0100

    take over Example.

--- DBSCAN-test/main/main.cpp
+++ DBSCAN-test/main/main.cpp
@@ -1,21 +1,73 @@
-/* C++ example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
+#include <stdio.h>
 #include <iostream>
+#include "dbscan.h"
 
 using std::cout;
 using std::endl;
+
+#define MINIMUM_POINTS 4     // minimum number of cluster
+#define EPSILON (0.75*0.75)  // distance for clustering, metre^2
+
+void readBenchmarkData(vector<Point>& points)
+{
+    // load point cloud
+    FILE *stream;
+    stream = fopen ("benchmark_hepta.dat","ra");
+
+	unsigned int num_points;
+	unsigned int cluster;
+	unsigned int i = 0;
+
+    fscanf(stream, "%u\n", &num_points);
+
+    Point *p = (Point *)calloc(num_points, sizeof(Point));
+
+    while (i < num_points)
+    {
+          fscanf(stream, "%f,%f,%f,%d\n", &(p[i].x), &(p[i].y), &(p[i].z), &cluster);
+          p[i].clusterID = UNCLASSIFIED;
+          points.push_back(p[i]);
+          ++i;
+    }
+
+    free(p);
+    fclose(stream);
+}
+
+void printResults(vector<Point>& points, int num_points)
+{
+    int i = 0;
+    printf("Number of points: %u\n"
+        " x     y     z     cluster_id\n"
+        "-----------------------------\n"
+        , num_points);
+    while (i < num_points)
+    {
+          printf("%5.2lf %5.2lf %5.2lf: %d\n",
+                 points[i].x,
+                 points[i].y, points[i].z,
+                 points[i].clusterID);
+          ++i;
+    }
+}
 
 /* Inside .cpp file, app_main function must be declared with C linkage */
 extern "C" void app_main(void)
 {
     cout << "app_main starting" << endl;
+    vector<Point> points;
+
+    // read point data
+    readBenchmarkData(points);
+
+    // constructor
+    DBSCAN ds(MINIMUM_POINTS, EPSILON, points);
+
+    // main loop
+    ds.run();
+
+    // result of DBSCAN algorithm
+    printResults(ds.m_points, ds.getTotalPointSize());    
 
 
 
